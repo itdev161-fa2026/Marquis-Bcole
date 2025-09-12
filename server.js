fa9f1833d9391bd52798a0ecaed7e576884b6d28
Marquis B-Cole <Browncom@gmatc.edu>
2025-09-11 20:17:24 -0500

    Completed Activity 3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express, { Router } from 'express';
 import connectDatabase from "./config/db.js";
-
+import {check, validationResult} from "express-validator";
 
 //int express app
 const app = express();
@@ -21,8 +21,18 @@ app.get ('/', (req, res)=>
  * @route
  * @desc
  */
-app.post("/api/users", (req, res) =>{
-    console.log(req.body);
+app.post("/api/users",
+     [check("name", "Please enter your name").not().isEmpty(),
+        check("email", "Please enter a valid email").isEmail(),
+        check("password", "please enter a password with 6 or more characters").isLength({min: 6})
+     ],
+     (req, res) =>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    } else {
+        return res.send(req.body);
+    }
     res.send(req.body);
 });
 
